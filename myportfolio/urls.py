f12bc7f6e77e4f01eea622b179370fa09e3618ee
Najoua ElFethi <80290373+najouaelfethi@users.noreply.github.com>
2024-11-07 00:52:19 +0100

    update

--- myportfolio/urls.py
+++ myportfolio/urls.py
@@ -2,7 +2,6 @@ from django.urls import path,include
 from .import views
 
 urlpatterns = [
-    path('', views.home),
     path('portfolio', views.portfolio),
     path('portfolio/', views.send_email_view, name='send_email_view'),
 ]
